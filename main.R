0c2a3bfcd91f541d736d3e10bab9740cc8fc00a1
RaySherb <ras0032@alumni.uah.edu>
2021-03-24 00:03:51 -0500

    plotting timeseries

--- main.R
+++ main.R
@@ -7,6 +7,24 @@ library(reshape2)
 # Load data (Training Set)
 kepler <- read_csv('exoTrain.csv')
 
+# Extract the label column
+kepler.labels <- kepler[,1]
+kepler <- kepler[,-1]
+
+# Rename columns
+set_names(kepler, 1:ncol(kepler))
+# Add a column of names to distinguish systems
+kepler$system <- as.factor(1:nrow(kepler))
+# Move system column to front
+kepler <- kepler %>% select(system, everything())
+
+# Gather into long format *worked without setting system to factor
+test <- kepler[kepler$system == 1, ] %>% gather(key = time, value = flux, -system) %>% 
+  ggplot(aes(x=time, y=flux))+
+  geom_point()
+
+#https://stackoverflow.com/questions/26536251/comparing-gather-tidyr-to-melt-reshape2
+#https://stackoverflow.com/questions/3777174/plotting-two-variables-as-lines-using-ggplot2-on-the-same-graph
 ##############################################################
 # Data description:
 # Timeseries data recording flux (light intensity) of stars
@@ -29,5 +47,5 @@ planets %>% ggplot(aes(x=1:nrow(planets))) +
 
 
 # Add row of time
-keplerTime <- 1:ncol(kepler) %>% rbind(kepler)
+kepler <- 1:ncol(kepler) %>% rbind(kepler)
 
