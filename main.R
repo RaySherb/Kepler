92b5271c2fd68ec0ebd68176c119a320fadf2c9d
RaySherb <ras0032@alumni.uah.edu>
2021-03-25 00:21:47 -0500

    standardization

--- main.R
+++ main.R
@@ -18,51 +18,25 @@ kepler <- read_csv('exoTrain.csv')
 kepler.labels <- kepler[,1]
 kepler <- kepler[,-1]
 
-# Scale features with Normalization (x-min)/(max-min)
-keplerN <- as_tibble(t(apply(kepler, 1, function(x){
-  (x-min(x))/(max(x)-min(x))
+# Scale features with Standardization (x-mu)/sigma
+keplerS <- as_tibble(t(apply(kepler, 1, function(x){
+  (x-mean(x))/sd(x)
 })))
+
+# Add column of min and max
+keplerS[, 'max'] <- apply(keplerS[, 1:ncol(keplerS)], 1, max)
+keplerS$min <- apply(keplerS, 1, min)
+# Move new columns to front
+keplerS <- keplerS %>% select(max, min, everything())
 
 
 # Gather into long format for plotting
-keplerG <- keplerN %>% mutate(system = 1:nrow(keplerN)) %>%
+keplerG <- keplerS[, -c(1:2)] %>% mutate(system = 1:nrow(keplerS)) %>%
   gather(key = time, value = flux, -system)
 # build a plot
-keplerG[keplerG$system %in% c(200,3), ] %>% ggplot(aes(x=time, y=flux))+
+keplerG[keplerG$system %in% c(200,3), ] %>% 
+  ggplot(aes(x=time, y=flux, color=as.factor(system)))+
   geom_point(alpha=0.1)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Rename columns
-# kepler <- set_names(kepler, 1:ncol(kepler))
-# Add a column of names to distinguish systems *This was causing problems with min/max function
-# kepler$system <- as.factor(1:nrow(kepler))
-
-# Add column of min and max
-kepler[, 'max'] <- apply(kepler[, 1:(ncol(kepler)-1)], 1, max)
-kepler$min <- apply(kepler, 1, min)
-# Move new columns to front
-kepler <- kepler %>% select(max, min, everything())
-
-
-
-
-
-
-
-#https://stackoverflow.com/questions/26536251/comparing-gather-tidyr-to-melt-reshape2
-#https://stackoverflow.com/questions/3777174/plotting-two-variables-as-lines-using-ggplot2-on-the-same-graph
 
 
 
